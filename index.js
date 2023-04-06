46ab04444dc7faf5c9c191a3cc449cf23b28b2df
Matt Pahuta <mhpahuta@gmail.com>
2023-04-06 16:10:06 -0700

    Add initial dynamic accounts rendering func

--- index.js
+++ index.js
@@ -2,59 +2,65 @@ const accounts = [
   {
     id: 1,
     title: "Main Account",
-    balance: "6700.56",
+    balance: 6700.56,
     spendings: [
       {
           category: "Rent",
-          spent: "1450"
+          spent: 1450
       },
       {
           category: "Groceries",
-          spent: "564"
+          spent: 564
       },
       {
           category: "Restaurants",
-          spent: "123"
+          spent: 123
       },
       {
           category: "Transport",
-          spent: "81"
+          spent: 81
       },
       {
           category: "Internet",
-          spent: "50"
+          spent: 50
       }
     ]
   },
   {
     id: 2,
     title: "Expenses",
-    balance: "5134.63",
+    balance: 5134.63,
     spendings: [
       {
           category: "Netflix",
-          spent: "19.99"
+          spent: 19.99
       },
       {
           category: "HBO Max",
-          spent: "14.99"
+          spent: 14.99
       },
       {
           category: "Setapp",
-          spent: "9.99"
+          spent: 9.99
       }
     ]
   },
   {
     id: 3,
     title: "Savings",
-    balance: "36500.12",
+    balance: 36500.12,
     spendings: []
   }
 ]
 
+const wrapper = document.getElementById('wrapper');
+const spendingsCol = document.getElementById('spendings-col')
 
-document.getElementById('accounts-inner').addEventListener('click', (e) => {
+document.getElementById('accounts').addEventListener('click', (e) => {
+
+  wrapper.classList.toggle('two-cols'); // update layout for two col view (1096px max-width)
+  spendingsCol.classList.toggle('hidden'); // show the spendings column
+
   if (e.target.id === 'main-account') {
     console.log('main account clicked')
   } else if (e.target.id === 'expenses-account') {
@@ -64,4 +70,30 @@ document.getElementById('accounts-inner').addEventListener('click', (e) => {
   }
 });
 
-// update wrapper max-width to 1096px 
+// page load - render account data
+
+function renderAccounts() {
+  const accountsCol = document.getElementById('accounts');
+  let accountsHtml = '';
+
+  accounts.forEach(account => {
+    console.log(account.id)
+    console.log(account.title)
+    console.log(account.balance)
+
+    accountsHtml += `
+      <div id="account-${account.id}" class="account selected">
+        <h3>${account.title}</h3>
+        <span>$ ${account.balance.toLocaleString()}</span>
+      </div>
+    `
+
+  })
+
+  accountsCol.innerHTML = accountsHtml;
+}
+
+renderAccounts(); // render accounts on page load
+
+// get correct spendings data 
+// accounts[i].spendings = array of objects
